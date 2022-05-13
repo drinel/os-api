4199f4deaf7806a7218f1a2393ba1d01c7a03ee9
drinel <will.i.am.002@hotmail.com>
2022-05-13 13:11:06 -0300

    adicionado tecnicos

--- src/main/java/com/order/os/services/DBService.java
+++ src/main/java/com/order/os/services/DBService.java
@@ -31,6 +31,13 @@ public class DBService {
 		Tecnico t1 = new Tecnico(null, "Catarino Segundo", "754.205.870-33", "(88)98989-9898");
 		
 		Tecnico t2 = new Tecnico(null, "Ubaldo Ponsio", "903.799.380-06", "(88)95545-7575");
+		
+		Tecnico t3 = new Tecnico(null, "Lindomario Perpétuo", "104.310.990-06", "(81)94040-2165");
+		
+		Tecnico t4 = new Tecnico(null, "Mayaro Carlos", "192.357.310-11", "(80)97290-7135");
+		
+		Tecnico t5 = new Tecnico(null, "Suzelen Pereira", "476.756.560-08", "(80)93368-4750");
+		
 
 		Cliente c1 = new Cliente(null, "Samuela Covas", "539.527.150-34", "(88)97979-7071");
 
@@ -39,7 +46,7 @@ public class DBService {
 		t1.getList().add(os1);
 		c1.getList().add(os1);
 
-		tecnicoRepository.saveAll(Arrays.asList(t1,t2));
+		tecnicoRepository.saveAll(Arrays.asList(t1,t2,t3,t4,t5));
 		clienteRepository.saveAll(Arrays.asList(c1));
 		osRepository.saveAll(Arrays.asList(os1));
 	}
